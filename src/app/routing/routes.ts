a9f3dd258a6a87b56627581bdcac63af9511797e
alokshakya <alokshakyacode@gmail.com>
2018-04-07 10:56:07 +0530

    routing anf coding component added working

--- src/app/routing/routes.ts
+++ src/app/routing/routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
 import { McqComponent } from '../mcq/mcq.component';
+import { CodingComponent } from '../coding/coding.component';
 
 export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'about',  component: AboutComponent },
-  { path: 'mcq',     component: McqComponent }
+  { path: 'mcq',     component: McqComponent },
+  { path: 'coding',     component: CodingComponent }
  
 ];
